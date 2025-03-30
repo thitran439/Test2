6181ad9c1347d673ed5f5002a4ac28beac783b1e
thitran439 <66186525+thitran439@users.noreply.github.com>
2025-03-30 18:54:30 -0400

    Initial Setup done

---
+++ src/test2/CustomerOrderTest.java
@@ -0,0 +1,14 @@
+package test2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerOrderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
